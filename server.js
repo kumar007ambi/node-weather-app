e09e53c8552bc8eca457f2e7481e46341f627aa7
Ambika <nirajkewat011@gmail.com>
2021-01-04 19:52:32 +0530

    make it better

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
 
 app.post('/', async (req, res) => {
     const city = req.body.city;
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.API_KEY}`;
 
     try {
         await fetch(url)
